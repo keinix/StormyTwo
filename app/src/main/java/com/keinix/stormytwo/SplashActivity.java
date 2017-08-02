7765a3bb685bd71e1642e2c73db3cb8db04539ae
Keinix <zackcosborn@gmail.com>
2017-08-02 10:50:56 +0900

    added spash activity for permission checks

--- app/src/main/java/com/keinix/stormytwo/SplashActivity.java
+++ app/src/main/java/com/keinix/stormytwo/SplashActivity.java
@@ -1,15 +1,14 @@
 package com.keinix.stormytwo;
 
-import android.*;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
