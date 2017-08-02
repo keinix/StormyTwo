be7a575377a95794c5a82622c685fd56d1cf4d4e
Keinix <zackcosborn@gmail.com>
2017-08-02 10:37:23 +0900

    Major location handling rework: app now generates new location data when none is cached. Still requires testing

--- app/src/main/java/com/keinix/stormytwo/MainActivity.java
+++ app/src/main/java/com/keinix/stormytwo/MainActivity.java
@@ -1,17 +1,15 @@
 package com.keinix.stormytwo;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -21,7 +19,12 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -40,7 +43,8 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     public final String TAG = MainActivity.class.getSimpleName();
     private CurrentWeather mCurrentWeather;
@@ -48,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
     private double mUserLatitude;
     private double mUserLongitude;
     private String mLocationName;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
 
 
     @BindView(R.id.timeLabel) TextView mTimeLabel;
@@ -67,66 +73,81 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         mProgressBar.setVisibility(View.INVISIBLE);
 
-
-
-        if (Build.VERSION.SDK_INT >= 23) {
-            locationPermissionRequest();
-
+        if (networkIsConnected()) {
+            // this starts a chain reaction where the weather will be updated on screen
+            // check the onConnected and onLocationChanged methods
+            setLocationResources();
         } else {
-            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-            getForcast();
+            Toast.makeText(this, "Connect to the internet please", Toast.LENGTH_LONG).show();
         }
 
         mRefreshImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getForcast();
+                refreshWeather();
             }
         });
 
     }
 
-    private void getForcast() {
 
-
-        if (networkIsConnected()) {
-
-            toggleRefresh();
-            getUserLocation();
-
-        } else {
-            GenericAlertDialog.newInstance("Oops!", "GET ON THE INTERNET NIGGA")
-                    .show(getFragmentManager(), "NotConnected");
+    private boolean networkIsConnected() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo= manager.getActiveNetworkInfo();
+        boolean isConneted = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isConneted = true;
         }
+        return isConneted;
     }
 
-    private void getUserLocation() {
+
+    private void setLocationResources() {
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mLocationRequest = new LocationRequest()
+                .setInterval(5000)
+                .setFastestInterval(1000)
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        mGoogleApiClient.connect();
+    }
+
+
+    private void refreshWeather() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                toggleRefresh();
+            }
+        });
+
         try {
-            mFusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            mUserLatitude = location.getLatitude();
-                            mUserLongitude = location.getLongitude();
-                            getLocationName();
-                            weatherApiCall();
-                        }
-                    });
+            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            if (location != null) {
+                parseLocation(location);
+                getNewForecast();
+            } else {
+                // location handled + weather updated in onLocationChanged()
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            }
+
         } catch (SecurityException e) {
-            Log.e(TAG, "No permission for getwork location");
+            Log.e(TAG, "No permission for network location");
             Toast.makeText(this, R.string.location_warning, Toast.LENGTH_LONG).show();
         }
     }
 
-    private void getLocationName() {
-        try {
-            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-            List<Address> addresses = geocoder.getFromLocation(mUserLatitude, mUserLongitude, 1);
-            mLocationName = addresses.get(0).getLocality();
-        } catch (IOException e) { e.printStackTrace(); }
-    }
-
-    private void weatherApiCall() {
+    private void getNewForecast() {
+        // makes a weather API call -> assigns data to model -> updates display
+        // make sure to toggelRefresh() before calling this method
         String apiKey ="4ada4e54ba9b437077c6695a8cdb202a";
         String forecastUrl = "https://api.darksky.net/forecast/" +
                 apiKey + "/" + mUserLatitude + "," + mUserLongitude;
@@ -140,21 +161,22 @@ public class MainActivity extends AppCompatActivity {
         Call call = client.newCall(request);
 
         call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                toggleRefresh();
-                GenericAlertDialog.newInstance("Oops!", "Could not get eather data")
-                        .show(getFragmentManager(), "NO_WEATHER_DATA");
-            }
 
             @Override
-            public void onResponse(Call call, Response response) throws IOException {
+            public void onFailure(Call call, IOException e) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         toggleRefresh();
                     }
                 });
+
+                GenericAlertDialog.newInstance("Oops!", "Could not get weather data")
+                        .show(getFragmentManager(), "NO_WEATHER_DATA");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
                 try {
                     String jsonData = response.body().string();
                     if (response.isSuccessful()) {
@@ -163,6 +185,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void run() {
                                 updateDisplay();
+                                toggleRefresh();
                             }
                         });
                     }
@@ -171,59 +194,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-
-    private void locationPermissionRequest() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 123);
-
-        } else {
-            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-            getForcast();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 123: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-                    getForcast();
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-                    Toast.makeText(this, "Please enable location access to use this app",
-                            Toast.LENGTH_LONG).show();
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
-
-    private boolean networkIsConnected() {
-        ConnectivityManager manager = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo= manager.getActiveNetworkInfo();
-        boolean isConneted = false;
-        if (networkInfo != null && networkInfo.isConnected()) {
-            isConneted = true;
-        }
-        return isConneted;
     }
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
@@ -267,5 +237,76 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void  parseLocation(Location location) {
+        mUserLatitude = location.getLatitude();
+        mUserLongitude = location.getLongitude();
 
+        try {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(mUserLatitude, mUserLongitude, 1);
+            mLocationName = addresses.get(0).getLocality();
+        } catch (IOException e) { e.printStackTrace(); }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+        Log.d("FINDME", "onResume connect activated");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d("FINDME", "The on connected method was activated");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                toggleRefresh();
+            }
+        });
+
+        try {
+            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            if (location != null) {
+                Log.d("FINDME", "getLastLocation was null");
+                parseLocation(location);
+                getNewForecast();
+            } else {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+                Log.d("FINDME", "Location updates requested");
+            }
+        } catch (SecurityException e) { e.printStackTrace(); }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended.");
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("FINDM", "onConnectionFailed method activated");
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("FINDME", "onLocationChanged activated");
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        parseLocation(location);
+        getNewForecast();
+
+    }
 }
---
+++ app/src/main/java/com/keinix/stormytwo/SplashActivity.java
@@ -0,0 +1,55 @@
+package com.keinix.stormytwo;
+
+import android.*;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity {
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        // SplashActivity is just for permission checks
+        intent = new Intent(this, MainActivity.class);
+
+
+        if (Build.VERSION.SDK_INT >= 23) {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, 123);
+            } else {
+                startActivity(intent);
+            }
+
+        } else {
+            startActivity(intent);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 123:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, "Sorry, This app needs network permissions to functions",
+                            Toast.LENGTH_LONG).show();
+                }
+        }
+
+    }
+}
